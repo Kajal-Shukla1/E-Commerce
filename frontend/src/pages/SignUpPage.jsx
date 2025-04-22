a44abf749b92f5e622e1583b1f749e9ef5b57333
Kajal Shukla <kajalshukla124@gmail.com>
2025-04-22 18:34:37 +0530

    Frontend second commit

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,24 +1,31 @@
+import {useState} from 'react';
+import { Link } from 'react-router-dom';
+import { UserPlus, Mail , Lock, ArrowRight, Loader } from 'lucide-react';
+import { motion } from 'framer-motion';
+
 const SignUpPage = () => {
-    return (
-        <div>
-        <h1>Sign Up</h1>
-        <form>
-            <div>
-            <label htmlFor="username">Username:</label>
-            <input type="text" id="username" name="username" required />
-            </div>
-            <div>
-            <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" required />
-            </div>
-            <div>
-            <label htmlFor="password">Password:</label>
-            <input type="password" id="password" name="password" required />
-            </div>
-            <button type="submit">Sign Up</button>
-        </form>
+    const loading = true;
+
+    const[formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+    }
+
+    return(
+        <div className='flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
+            <motion.div initial={{ opacity: 0, y:20 }} animate={{ opacity: 1, y:0 }} transition={{ duration: 0.8 }}>
+                <h2 className='mt-6 text-3xl font-extrabold text-emerald-400 text-center'>Create your account</h2>
+            </motion.div>
+
         </div>
-    );
+    )
 }
 
 export default SignUpPage;
