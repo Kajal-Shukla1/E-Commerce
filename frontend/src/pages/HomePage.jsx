9c97e3f23f48cfe204b967b575c779616779de74
Kajal Shukla <kajalshukla124@gmail.com>
2025-04-28 18:08:57 +0530

    Home page first commit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,36 @@
+import CategoryItem from "../components/CategoryItem";
+
+const categories = [
+  { href: "/jeans",name: "Jeans", imageUrl: "/jeans.jpg" },
+  { href: "/tshirts", name: "T-shirts", imageUrl: "/tshirts.jpg" },
+  { href: "/shoes", name: "Shoes", imageUrl: "/shoes.jpg" },
+  { href: "/glasses", name: "Glasses", imageUrl: "/glasses.png" },
+  { href: "/jackets", name: "Jackets", imageUrl: "/jackets.jpg" },
+  { href: "/sweaters", name: "Sweaters", imageUrl: "/sweaters.jpg" },
+  { href: "/hats", name: "Hats", imageUrl: "/hats.jpg" },
+  { href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
+];
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main content of the home page.</p>
+    <div className="relative min-h-screen text-white overflow-hidden">
+      <div className="relative z-10 flex max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 display-inherit">
+        <h1 className="text-5xl font-bold text-center sm:text-6xl text-emerald-400 mb-4">
+          Explore Our Collections
+        </h1>
+        <p className="text-xl text-center text-gray-300 mb-12">
+          Discover the latest trends and styles in fashion. Shop now and elevate your wardrobe with our curated collections.
+        </p>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+          {categories.map((category) => (
+            <CategoryItem
+              category={category}
+              key={category.name}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
