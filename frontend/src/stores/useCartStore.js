9963b21dd0742b4940ecc6b232889c064f849ab2
Kajal Shukla <kajalshukla124@gmail.com>
2025-05-03 22:22:58 +0530

    Add to Cart first commit

--- frontend/src/stores/useCartStore.js
+++ frontend/src/stores/useCartStore.js
@@ -12,6 +12,7 @@ export const useCartStore = create((set, get) => ({
         try {
             const res = await axios.get("/cart");
             set({cart:res.data})
+            get().calculateTotals();
         } catch (error) {
             set({cart : []})
             toast.error(error.response.data.message || "An error occured");
@@ -29,9 +30,23 @@ export const useCartStore = create((set, get) => ({
                 ? state.cart.map((item) => (item._id === product._id ? {...item,quantity: item.quantity + 1}: item))
                 : [...state.cart , {product , quantity:1}];
                 return {cart : newCart};
-            })
+            });
+            get().calculateTotals();
         } catch (error) {
          toast.error(error.response.data.message || "An error occured");   
         }
+    },
+
+    calculateTotals: () =>{
+        const {cart, coupon} = get();
+        const subtotal = cart.reduce((sum,item) => sum+ item.price * item.quantity,0);
+        let total = subtotal;
+
+        if(coupon){
+            const discount = subtotal * (coupon.discountPercentage / 100);
+            total = subtotal - discount;
+        }
+
+        set({subtotal, total});
     }
 }))
