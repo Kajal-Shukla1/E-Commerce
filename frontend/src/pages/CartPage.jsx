8799ff4dd34dc6178a42d8e158df005aa15f7833
Kajal Shukla <kajalshukla124@gmail.com>
2025-05-07 22:15:41 +0530

    Add to Cart 2nd commit

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -2,9 +2,11 @@ import {useCartStore} from "..stores/useCartStore"
 const CartPage = () => {
   const {cart} = useCartStore();
   return (
-    <div className="">
-      <div>
+    <div className="py-8 md:py-16">
+      <div className="mx-auto max-w-screen-xl px-4 2xl:px-0">
+        <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
 
+        </div>
       </div>
     </div>
   )
